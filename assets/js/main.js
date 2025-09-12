e22bba8ef1f59edf7f15f26fe7bf9736b9b787ad
Mohammed Althaf T K <mohammedalthaf1111@gmail.com>
2025-09-12 12:58:14 +0530

    animations updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,10 +53,48 @@ const swiperPopular = new Swiper('.popular__swiper', {
 
 
 /*=============== SHOW SCROLL UP ===============*/
-
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr=ScrollReveal({
+    origin:'top',
+    distance:'60px',
+    duration:2500,
+    delay:300
+})
 
+
+sr.reveal(`.home__data, .popular__container, .footer`)
+sr.reveal(`.home__board`,{delay:700, distance:'100px', origin:'right'})
+sr.reveal(`.home__pizza`,{delay:1400, distance:'100px', origin:'bottom', rotate:{z:-90}})
+sr.reveal(`.home__ingredient`,{delay:2000, interval:100})
+sr.reveal(`.about__data, .recipe__list, .contact__data`,{origin:'right'})
+sr.reveal(`.about__img, .recipe__img, .contact__image`,{origin:'left'})
+sr.reveal(`.products__card`,{interval:100})
